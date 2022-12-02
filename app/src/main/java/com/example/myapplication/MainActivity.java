2d6b91b26bcb587142c875a2e478b392299a9c40
bilalgtc <bilal.ansari@gtcsys.com>
2022-12-02 15:25:51 +0530

    Toast multiple

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -12,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "Hello world", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Hello world", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Hello world", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Hello world", Toast.LENGTH_SHORT).show();
     }
 }
